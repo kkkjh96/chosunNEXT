23235d551c47a7eaed519a52d39b732d5485266f
pmg <dkzld159@naver.com>
2025-02-03 21:33:54 +0900

    cms 레이아웃 make

---
+++ src/main/java/com/example/chosunnext/controller/CmsController.java
@@ -0,0 +1,20 @@
+package com.example.chosunnext.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("cms")
+public class CmsController {
+
+    @GetMapping("/")
+    public String cms() {
+        return "/cms/main";
+    }
+
+}
