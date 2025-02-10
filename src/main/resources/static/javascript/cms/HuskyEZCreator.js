56a12e085d4f3ce53928f9e1631510f6cf892b02
pmg <dkzld159@naver.com>
2025-02-10 19:04:26 +0900

    cms 소분류 대분류

--- src/main/resources/static/javascript/cms/HuskyEZCreator.js
+++ src/main/resources/static/javascript/cms/HuskyEZCreator.js
@@ -2,6 +2,7 @@ var oEditors = [];
 nhn.husky.EZCreator.createInIFrame({
     oAppRef: oEditors,
     elPlaceHolder: "smarteditor", // 에디터가 들어갈 ID
-    sSkinURI: "./SmartEditor2Skin.html",  // 스킨 파일 위치 (현재 HTML 파일과 같은 폴더)
+    sSkinURI: "/static/javascript/cms/SmartEditor2Skin.html",
+    // 스킨 파일 위치 (현재 HTML 파일과 같은 폴더)
     fCreator: "createSEditor2"
 });
